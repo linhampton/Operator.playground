24d1d40d502b46e9f8731ed9d0d506c38f133aaf
linhampton <prc-lh138-17@PRC-LH138-17s-iMac.local>
2016-07-11 16:07:20 -0500

    OperatorCalc
    This is the calculator

---
+++ Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+// Create a calculator playground into 3 sections
+// Lin Hampton 7-11-2016
+
+
+import UIKit
+
+//Top Section defines input variables
+
+var a = 1
+var b = 4
+var c = 7
+var addit = 0
+var subit = 0
+var multit = 0
+var divit = 0
+
+//define all of the operations
+
+addit = a + b
+subit = a - b
+multit = a * b
+divit = a/b
+
+//print results to the screen 
+
+print("a + b = ", addit, terminator:"")
+print("a - b = ", subit, terminator:"")
+print("a * b = ", multit, terminator:"")
+print("a / b = ", divit, terminator:"")
+
+
